6c8e97ca80180c15e9e5753dcefd1e8151e93f22
Erick Vargas <erickkvargas@gmail.com>
2022-05-17 15:37:20 -0700

    commit tree 09a0214342e539ca2b51705570a84b881b15ee1d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    #setter
+    def name=(name)
+        @name = name
+      end
+    
+      # getter method
+      def name
+        @name
+      end
+
+      def breed
+        @breed
+      end 
+
+      def breed=(breed)
+        @breed=breed
+      end 
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    #this is the getter 
+    def name
+        @name
+    end 
+    #this is the setter
+    def name=(name)
+        @name=name
+    end 
+
+    def job
+        @job
+    end 
+    def job=(job)
+        @job=job
+    end 
+    
 
 end
